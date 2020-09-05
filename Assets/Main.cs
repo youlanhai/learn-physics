052ca6459ad7d25f881115d0ea142987031d17e3
youlanhai <you_lan_hai@foxmail.com>
2020-09-05 13:52:13 +0800

    增加工程之间的跳转

---
+++ Assets/Main.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public Transform listContent;
+    public GameObject itemPrefab;
+
+    string[] simples = new string[]
+    {
+        "2.分离轴碰撞检测", "02-sat",
+        "3.GJK碰撞检测基础", "03-gjk",
+        "4.GJK计算多边形最近距离", "04-gjk-closest-point",
+        "5.GJK&EPA计算穿透向量", "05-gjk-epa",
+    };
+
+    private void Start()
+    {
+        itemPrefab.SetActive(true);
+        for(int i = 0; i < simples.Length; i += 2)
+        {
+            CreateListItem(simples[i], simples[i + 1]);
+        }
+        itemPrefab.SetActive(false);
+    }
+
+    GameObject CreateListItem(string title, string sceneName)
+    {
+        GameObject item = Instantiate(itemPrefab, listContent, false);
+
+        Button button = item.GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            OpenScene(sceneName);
+        });
+
+        Text text = item.transform.Find("Text").GetComponent<Text>();
+        text.text = title;
+        return item;
+    }
+
+    public void OpenScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void OnBtnCSDN()
+    {
+        Application.OpenURL("https://blog.csdn.net/you_lan_hai");
+    }
+
+    public void OnBtnGithub()
+    {
+        Application.OpenURL("https://github.com/youlanhai");
+    }
+}
---
+++ Assets/ReturnHome.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReturnHome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnBtnReturn);
+    }
+
+    void OnBtnReturn()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+    }
+}
